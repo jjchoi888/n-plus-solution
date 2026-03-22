820dc2c0b4d0e53926c4967593761750bd82403a
내 깃허브 아이디 (예: jjchoi888) <jjchoi888@gmail.com>
2026-03-22 14:12:58 +0800

    server connect

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -3,11 +3,17 @@ const nextConfig = {
   async rewrites() {
     return [
       {
+        // 1. 일반 데이터 API 통로
         source: '/api/:path*',
-        // 💡 목적지를 열려있는 8000번으로!
         destination: 'http://136.117.49.111:8000/api/:path*',
+      },
+      {
+        // 2. 실시간 소켓 통신 통로 (이게 없으면 소켓 에러가 납니다)
+        source: '/socket.io/:path*',
+        destination: 'http://136.117.49.111:8000/socket.io/:path*',
       },
     ];
   },
 };
+
 export default nextConfig;
